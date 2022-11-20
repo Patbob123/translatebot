6c7ec77c180e75889e67429d50fe85613b88274d
unknown <flyingbikeddd@gmail.com>
2022-11-19 23:48:00 -0500

    i got madd

--- commands/help.js
+++ commands/help.js
@@ -3,18 +3,18 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('bot syntax')
-        ,
+    ,
     async execute(interaction) {
         let x = new EmbedBuilder()
-            .setColor(0x0099FF)
+            .setColor(0xb411fa)
             .setTitle("Commands")
             .addFields(
                 { name: '/help', value: 'help' },
-                { name: '/language', value: 'configure language'},
-                { name: '/toggle', value: 'toggle translator'},
-                { name: '/madlib', value: 'start a madlib'},
-                { name: '/rhyme', value: 'find rhyming words'},
-                { name: '/synonyms', value: 'find synonyms'},
+                { name: '/language', value: 'configure language' },
+                { name: '/toggle', value: 'toggle translator' },
+                { name: '/madlib', value: 'start a madlib' },
+                { name: '/rhyme', value: 'find rhyming words' },
+                { name: '/synonyms', value: 'find synonyms' },
             )
             .setTimestamp()
         await interaction.reply({ embeds: [x] });
--- commands/madlibs.js
+++ commands/madlibs.js
@@ -2,6 +2,49 @@ const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const fetch = require('cross-fetch');
 const API = 'http://madlibz.herokuapp.com/api/random';
 
+const filter = m => m;
+
+async function questionAndAnswers(interaction, madlib, index, answers) {
+    if (index >= madlib.blanks.length) {
+        let storytext = madlib.value[0]
+        for(let i =0;i<answers.length;i++){
+            storytext += `${answers[i]}${madlib.value[i+1]}`
+        }
+        let story = new EmbedBuilder()
+        .setColor(0xb411fa)
+        .setTitle(madlib.title)
+        .setDescription(storytext)
+        await interaction.followUp({ embeds: [story]})
+        return
+    }
+    let question = new EmbedBuilder()
+        .setColor(0xb411fa)
+        .setTitle(`Enter ${madlib.blanks[index]}!`)
+    await interaction.followUp({ embeds: [question], fetchReply: true })
+        .then(() => {
+            interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
+                .then(collected => {
+                    if(collected.first().content == "!stop"){
+                        let stopped = new EmbedBuilder()
+                        .setColor(0xb411fa)
+                        .setTitle(`Madlib stopped`)
+                        interaction.followUp({embeds: [stopped]});
+                        return
+                    }
+                    answers.push(collected.first())
+                    return questionAndAnswers(interaction, madlib, index + 1, answers);
+
+                })
+                .catch(collected => {
+                    let timeout = new EmbedBuilder()
+                        .setColor(0xb411fa)
+                        .setTitle(`30 Second Time Out`)
+                    interaction.followUp({embeds: [timeout]});
+                });
+        });
+
+
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -22,24 +65,14 @@ module.exports = {
         }).catch(err => {
         })
 
+
         let x = new EmbedBuilder()
             .setColor(0xb411fa)
             .setTitle(`Mystery madlib started!`)
         await interaction.reply({ embeds: [x] });
 
 
-        let question = new EmbedBuilder()
-            .setColor(0xb411fa)
-            .setTitle(`Enter ${blanks[index]}!`)
-        await interaction.followUp({ content: { embeds: [question] }, fetchReply: true })
-            .then(() => {
-          
-            });
-        console.log(answers)
-        // for(let i =0;i<madlib.blanks.length;i++){
-
-        // }
-        // let answers = await questionAndAnswers(interaction, madlib.blanks[i], 0, [])
+        let answers = await questionAndAnswers(interaction, madlib, 0, [])
 
         // let story = madlib.value[0]
         // for(let i =0;i<answers.length;i++){
@@ -49,28 +82,6 @@ module.exports = {
 
         //value
     },
-    async questionAndAnswers(interaction, blanks, index, answers) {
-        if (index > blanks.length) {
-            return answers
-        }
-        let question = new EmbedBuilder()
-            .setColor(0xb411fa)
-            .setTitle(`Enter ${blanks[index]}!`)
-        await interaction.followUp({ content: { embeds: [question] }, fetchReply: true })
-            .then(() => {
-                interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-                    .then(collected => {
-                        answers.push(collected)
-                        questionAndAnswers(interaction, blanks, index + 1)
-                    })
-                    .catch(collected => {
-                        let timeout = new EmbedBuilder()
-                            .setColor(0xb411fa)
-                            .setTitle(`Time out!`)
-                        interaction.channel.send({ embeds: [timeout] })
-                    });
-            });
 
-    }
 };
 
